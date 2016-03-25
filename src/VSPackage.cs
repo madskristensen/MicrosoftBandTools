fc5932be6e400463f347a7d67abe48d0c48a516a
Mads Kristensen <post@madskristensen.net>
2016-03-25 12:28:46 -0700

    Refactorings

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -6,19 +6,17 @@ using Microsoft.VisualStudio.Shell.Interop;
 namespace MicrosoftBandTools
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", Version, IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
     [Guid(PackageGuidString)]
     public sealed class VSPackage : Package
     {
         public const string PackageGuidString = "67ca5344-cb3f-47f8-aca5-666f9b149cef";
-        public const string Version = "1.0";
-        public const string Title = "Microsoft Band Tools";
 
         protected override void Initialize()
         {
-            Logger.Initialize(this, Title);
-            Telemetry.Initialize(this, Version, "c04811f5-2c73-460b-996e-225918fa8ba0");
+            Logger.Initialize(this, Vsix.Name);
+            Telemetry.Initialize(this, Vsix.Version, "c04811f5-2c73-460b-996e-225918fa8ba0");
             base.Initialize();
         }
     }
