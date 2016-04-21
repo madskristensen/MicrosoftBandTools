814308e0aa7c3672de20aabb5383e2dac729bb7e
Mads Kristensen <post@madskristensen.net>
2016-04-21 10:19:33 -0700

    Added VsixLogger nuget package

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -8,15 +8,12 @@ namespace MicrosoftBandTools
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
-    [Guid(PackageGuidString)]
+    [Guid("67ca5344-cb3f-47f8-aca5-666f9b149cef")]
     public sealed class VSPackage : Package
     {
-        public const string PackageGuidString = "67ca5344-cb3f-47f8-aca5-666f9b149cef";
-
         protected override void Initialize()
         {
-            Logger.Initialize(this, Vsix.Name);
-            Telemetry.Initialize(this, Vsix.Version, "c04811f5-2c73-460b-996e-225918fa8ba0");
+            Logger.Initialize(this, Vsix.Name, Vsix.Version, "c04811f5-2c73-460b-996e-225918fa8ba0");
             base.Initialize();
         }
     }
